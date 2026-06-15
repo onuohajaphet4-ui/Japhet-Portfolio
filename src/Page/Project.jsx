a5d46ad598d24739c6ce54c42a618958392b05ca
onuohajaphet4 <onuohajaphet4@gmail.com>
2026-06-15 04:11:33 -0700

    added neww project

--- src/Page/Project.jsx
+++ src/Page/Project.jsx
@@ -4,6 +4,7 @@ import project1 from '../assets/project1.png'
 import project7 from '../assets/project7.png'
 import project3 from '../assets/yati.png'
 import pastry from '../assets/pastry.jpg'
+import aegis from '../assets/Aegis.png'
 import More from '../Component/More'
 import {Link} from 'react-router-dom'
 const Skill = () => {
@@ -15,6 +16,34 @@ const Skill = () => {
       <h2>
         Projects
       </h2>
+      
+      <div className="contact-card">
+        <div className="con-img">
+        <img src={aegis} alt="" />
+        </div>
+
+        <div className="con-info">
+          <h1>AEGIS LEGAL PARTNERS</h1>
+          <p>Professional Law Firm Website</p>
+
+          <div className="conflex1">
+             <h4>React</h4>
+             <h4>Tailwind Css</h4> 
+          </div>
+
+           <div className="conflex1">
+             <h4>Express</h4>
+             <h4>MongoDb</h4>
+          </div>
+
+          <div className="conflex2">
+            <h4 className='h44 slide-hover'><a href="https://aegis-legal.vercel.app/" style={{textDecoration:'none' , color:'inherit'}}>Live</a></h4>
+            <h4  className='h44 slide-hover'><a href="https://github.com/onuohajaphet4-ui/AEGIS-LEGAL" style={{textDecoration:'none' , color:'inherit'}}>Github</a></h4>
+          </div>
+
+          <h4 className='con-h4 slide-hover'><Link to='/zynox' style={{textDecoration:'none' , color:'inherit'}}>Project Details</Link></h4>
+        </div>
+      </div>
 
       <div className="contact-card">
         <div className="con-img">
